641e8a6d6a471923a7a54980e42c4d4234b3a4a0
deniselim <denise.lim.sh@gmail.com>
2017-03-06 02:26:34 +0800

    Amend starting template

---
+++ src/main/java/com/mkyong/helloworld/service/HelloWorldService.java
@@ -0,0 +1,13 @@
+package com.mkyong.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+}
--- src/main/java/com/mkyong/helloworld/web/WelcomeController.java
+++ src/main/java/com/mkyong/helloworld/web/WelcomeController.java
@@ -2,17 +2,10 @@ package com.mkyong.helloworld.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
 
 	private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
-
-	@Autowired
-	public WelcomeController() {
-	}
 }
